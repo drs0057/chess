69876a6beb56e2087a2e3501ac53e1ab6523b942
Derek Swanson <drs0057@auburn.edu>
2023-03-20 12:55:37 -0500

    Expanded on squares and board classes, 8x8 board printed successfully.

--- board.py
+++ board.py
@@ -4,14 +4,50 @@ class Square:
     """A class representing each square."""
 
     # Initialize attributes
-    def __init__(self, x, y, color):
-        
-        # Absolute positions of the top-left corner
-        self.x = x
-        self.y = y
-        self.width = 10
-        self.height = 10
-        self.pos_size = (x, y, self.width, self.height)
-        self.color = color
-        self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
+    def __init__(self, x_num, y_num, screen):
 
+        # Denotes coordinates of the square (0 - 7 in both dimentions)
+        self.x_num = x_num
+        self.y_num = y_num
+        # Absolute positions of the top-left corner of the square
+        self.x_abs = x_num * 100
+        self.y_abs = y_num * 100
+        # Define color
+        if (self.x_num + self.y_num) % 2 == 0:
+            self.color = (237, 199, 190) # Light square
+        elif self.x_num + self.y_num % 2 != 0:
+            self.color = (115, 88, 81) # Dark square
+        # Screen to draw the square on
+        self.draw_screen = screen
+        # Rect class parameters: (abs. x_coor of top left corner, 
+        # abs. y_coor of top left corner, width of rect, height of rect)
+        self.rect = pygame.Rect(self.x_abs, self.y_abs, 100, 100)
+
+    # Method to draw individual square
+    def draw(self):
+        # pygame.draw.rect() parameters: (surface to draw on, color, 
+        # object to draw)
+        pygame.draw.rect(self.draw_screen, self.color, self.rect)
+
+
+
+class Board:
+    """A class representing the board as a whole."""
+
+    # Initilaize attributes
+    def __init__(self):
+        self.placeholder = 1
+
+    def build_square_objects(self, screen):
+        """Returns a list of square objects."""
+        squares = []
+        for y_num in range(8):
+            for x_num in range(8):
+                squares.append(Square(x_num, y_num, screen))
+        return squares
+    
+    def print_squares(self, squares):
+        """Takes a list of square objects and prints them all. """
+        for square in squares:
+            square.draw()
+        
--- game.py
+++ game.py
@@ -1,18 +1,38 @@
 import pygame
 from board import *
 
-# Main game loop
+# Initialize the game and needed objects
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((800, 800))
+pygame.display.set_caption('Chess')
 
+# bishop = pygame.image.load('images/bishop_white.png')
+# bishop = pygame.transform.scale(bishop, (100, 100))
+# queen = pygame.image.load('images/queen_white.png')
+
+# Main game loop
 running = True
-# while running:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             runnning = False
-screen.fill((255, 0, 0))
-pygame.display.update()
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
 
-# square = Square(10, 10, (255, 0, 0))
+    # Build the board
+    board = Board()
+    squares = board.build_square_objects(screen)
+    board.print_squares(squares)
+
+    # Game logic
+    # screen.fill((38, 41, 64))
+
+
+    # screen.blit(bishop, (10, 10))
+    # screen.blit(queen, (200, 200))
+    
+    pygame.display.update()
+
+
+
 # pygame.draw.rect(screen, square.color, square.pos_size)
 # pygame.display.update()
